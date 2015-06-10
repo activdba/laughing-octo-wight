8566ff378fe5ac67375b138c94727e49855c3fb9
activdba <nitin.s.chauhan@gmail.com>
2015-06-10 16:28:57 +0530

    Changed Parsing of JSON to include DR details

--- chklist_db_os.py
+++ chklist_db_os.py
@@ -103,182 +103,184 @@ uid = "system"
 host = data["DBPRICONNDET"]["HOST"]
 port = data["DBPRICONNDET"]["PORT"]
 
-duid = "sys"
+druid = "sys"
 drhost = data["DBDRCONNDET"]["HOST"]
 drport = data["DBDRCONNDET"]["PORT"]
 
 output_json = json.load(open('reqinfo.json'))
+#for majorkey, subdict in output_json.items():
+#	if majorkey == 'DBPRIINFO':
+#		for dbname, passwd in subdict.items():
+#			connection = cx_Oracle.connect(uid + "/" + passwd + "@" + host + ":" + port + "/" + dbname)
+#			cursor = connection.cursor()
+
 for majorkey, subdict in output_json.items():
 	if majorkey == 'DBPRIINFO':
-		for dbname, passwd in subdict.items():
-			connection = cx_Oracle.connect(uid + "/" + passwd + "@" + host + ":" + port + "/" + dbname)
-			cursor = connection.cursor()
+		print("Majorkey is",majorkey)
+		print("Subdict is ",subdict)
+		for db_name_details in subdict:
+			print("db name and its details are ",db_name_details)
+			for dbname, dbdetails in db_name_details.items():
+				print("dbname is ",dbname)
+				print("PRI DB PASS",dbdetails[0])
+				passwd = dbdetails[0]
+				print("DR DB NAME",dbdetails[1])
+				drname = dbdetails[1]
+				print("DR DB PASS",dbdetails[2])
+				drpwd = dbdetails[2]
+				connection = cx_Oracle.connect(uid + "/" + passwd + "@" + host + ":" + port + "/" + dbname)
+				cursor = connection.cursor()
+				drconnection = cx_Oracle.connect(druid + "/" + drpwd + "@" + drhost + ":" + drport + "/" + drname, mode = cx_Oracle.SYSDBA)
+				drcursor = drconnection.cursor()
 
 # Create worksheet
-			dbrow=1
-			rrow=16
-			trow=28
-			tcol=-1
-			syncol=4
-			drcol=4
-
-			worksheet = workbook.add_worksheet(dbname)
-			
-			worksheet.set_column('A:A', 25)
-			worksheet.set_column('B:B', 18)
-			worksheet.set_column('C:C', 18)
-			worksheet.set_column('D:D', 19)
-			worksheet.set_column('E:E', 10)
-			worksheet.set_column('F:F', 15)
-			worksheet.set_column('G:G', 15)
-			worksheet.set_column('H:H', 25)
-			worksheet.set_column('I:I', 15)
-			worksheet.set_column('K:K', 25)
-			
-			now = time.strftime("%c")
-			## date and time representation
-			#print ("Current date & time " + time.strftime("%c"))
-			
-			worksheet.merge_range('A1:B1', now, merge_format)
-			worksheet.merge_range('A15:B15','PERFORMANCE INFORMATION', merge_format)
-			worksheet.merge_range('A25:D25','TABLESPACE INFORMATION', merge_format)
-			worksheet.merge_range('D6:I6','DAILY BACKUP INFORMATION',merge_format)
-			worksheet.merge_range('D7:E7','RMAN BACKUP STATUS :',merge_format2)
-			worksheet.merge_range('D8:E8','DATAPUMP BACKUP STATUS :',merge_format2)
-			worksheet.merge_range('D1:H1','DR SYNC STATUS',merge_format)
-			worksheet.merge_range('D2:D4','SYNC STATUS',merge_format)
-			worksheet.merge_range('F16:K16','MOUNTPOINT SPACE INFORMATION',merge_format)
-			
-			worksheet.write(1, 0, 'DATABASE NAME :',att_format)
-			worksheet.write(2, 0, 'OPEN MODE :',att_format)
-			worksheet.write(3, 0, 'DATABASE ID :',att_format)
-			worksheet.write(4, 0, 'CREATED :',att_format)
-			worksheet.write(5, 0, 'LOG MODE :',att_format)
-			worksheet.write(6, 0, 'HOSTNAME :',att_format)
-			worksheet.write(7, 0, 'VERSION :',att_format)
-			worksheet.write(8, 0, 'SGA SIZE (MB) :',att_format)
-			worksheet.write(9, 0, 'PGA SIZE (MB) :',att_format)
-			worksheet.write(10, 0, 'PHYSICAL SIZE (GB) :',att_format)
-			worksheet.write(11, 0, 'LOGICAL SIZE (GB) :',att_format)
-			worksheet.write(12, 0, 'UPTIME (DAYS) :',att_format)
-			
-			worksheet.write(16, 0, 'SORT RATIO :',att_format)
-			worksheet.write(17, 0, 'DICTIONARY HIT RATIO :',att_format)
-			worksheet.write(18, 0, 'LIBRARY CACHE HIT RATIO :',att_format)
-			worksheet.write(19, 0, 'BUFFER CACHE HIT RATIO :',att_format)
-			worksheet.write(20, 0, 'INDEX LOOKUP RATIO :',att_format)
-			worksheet.write(21, 0, 'REDO LOG HIT RATIO :',att_format)
-			worksheet.write(22, 0, 'NUMBER OF TRANSACTIONS :',att_format)
-			
-			worksheet.write(26, 0, 'TABLESPACE NAME',att_format2)
-			worksheet.write(26, 1, 'TOTAL SIZE(MB)',att_format2)
-			worksheet.write(26, 2, 'USED SPACE(MB)',att_format2)
-			worksheet.write(26, 3, 'AVAILABLE SPACE(MB)',att_format2)
-			
-			worksheet.write(1, 4, 'DATABASE',att_format)
-			worksheet.write(1, 5, 'SEQUENCE #',att_format)
-			worksheet.write(1, 6, 'ARCHIVED',att_format)
-			worksheet.write(1, 7, 'APPLIED',att_format)
-			worksheet.write(2, 4, 'PRIMARY :',att_format)
-			worksheet.write(3, 4, 'STANDBY :',att_format)
-			
-			worksheet.write('F8', 'BACKUP DATE',att_format)
-			worksheet.write('G8', 'CLIENT',att_format)
-			worksheet.write('H8', 'SCHEMA_NAME',att_format)
-			worksheet.write('I8', 'STATUS',att_format)
-			
-			worksheet.write('F17', 'FILESYSTEM',att_format)
-			worksheet.write('G17', 'TOTAL SIZE(GB)',att_format)
-			worksheet.write('H17', 'USED(GB)',att_format)
-			worksheet.write('I17', 'FREE(GB)',att_format)
-			worksheet.write('J17', 'USED(%)',att_format)
-			worksheet.write('K17', 'MOUNTED ON',att_format)
-			
-			# ways to connect 
-			# 1st way
-			#connection_String = 'system/infotech@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.99.38)(PORT=3128)))(CONNECT_DATA=(SID=servname)))'
-			#connection = cx_Oracle.connect(connection_String)
-			
-			# 2nd way
-			#connection = cx_Oracle.connect('system/infotech@192.168.99.38:3128/servname')
-			
-			# 3rd way
+				dbrow=1
+				rrow=16
+				trow=28
+				tcol=-1
+				syncol=4
+				drcol=4
 	
-	#for majorkey, subdict in output_json.items():
-	# if majorkey == servname:
-	#  for dbnam, passwd in subdict.items():
-	#   dpwd = passwd
-	#   drconnection = cx_Oracle.connect(druid + "/" + drpwd + "@" + drhost + ":" + drport + "/" + drname, mode = cx_Oracle.SYSDBA)
-	#   drcursor = drconnection.cursor()
-	
-			for dbparameter in dbparameters:
-				if 'created' in dbparameter:
-					cursor.execute(dbparameter)
+				worksheet = workbook.add_worksheet(dbname)
+				
+				worksheet.set_column('A:A', 25)
+				worksheet.set_column('B:B', 18)
+				worksheet.set_column('C:C', 18)
+				worksheet.set_column('D:D', 19)
+				worksheet.set_column('E:E', 10)
+				worksheet.set_column('F:F', 15)
+				worksheet.set_column('G:G', 15)
+				worksheet.set_column('H:H', 25)
+				worksheet.set_column('I:I', 15)
+				worksheet.set_column('K:K', 25)
+				
+				now = time.strftime("%c")
+				## date and time representation
+				#print ("Current date & time " + time.strftime("%c"))
+				
+				worksheet.merge_range('A1:B1', now, merge_format)
+				worksheet.merge_range('A15:B15','PERFORMANCE INFORMATION', merge_format)
+				worksheet.merge_range('A25:D25','TABLESPACE INFORMATION', merge_format)
+				worksheet.merge_range('D6:I6','DAILY BACKUP INFORMATION',merge_format)
+				worksheet.merge_range('D7:E7','RMAN BACKUP STATUS :',merge_format2)
+				worksheet.merge_range('D8:E8','DATAPUMP BACKUP STATUS :',merge_format2)
+				worksheet.merge_range('D1:H1','DR SYNC STATUS',merge_format)
+				worksheet.merge_range('D2:D4','SYNC STATUS',merge_format)
+				worksheet.merge_range('F16:K16','MOUNTPOINT SPACE INFORMATION',merge_format)
+				
+				worksheet.write(1, 0, 'DATABASE NAME :',att_format)
+				worksheet.write(2, 0, 'OPEN MODE :',att_format)
+				worksheet.write(3, 0, 'DATABASE ID :',att_format)
+				worksheet.write(4, 0, 'CREATED :',att_format)
+				worksheet.write(5, 0, 'LOG MODE :',att_format)
+				worksheet.write(6, 0, 'HOSTNAME :',att_format)
+				worksheet.write(7, 0, 'VERSION :',att_format)
+				worksheet.write(8, 0, 'SGA SIZE (MB) :',att_format)
+				worksheet.write(9, 0, 'PGA SIZE (MB) :',att_format)
+				worksheet.write(10, 0, 'PHYSICAL SIZE (GB) :',att_format)
+				worksheet.write(11, 0, 'LOGICAL SIZE (GB) :',att_format)
+				worksheet.write(12, 0, 'UPTIME (DAYS) :',att_format)
+				
+				worksheet.write(16, 0, 'SORT RATIO :',att_format)
+				worksheet.write(17, 0, 'DICTIONARY HIT RATIO :',att_format)
+				worksheet.write(18, 0, 'LIBRARY CACHE HIT RATIO :',att_format)
+				worksheet.write(19, 0, 'BUFFER CACHE HIT RATIO :',att_format)
+				worksheet.write(20, 0, 'INDEX LOOKUP RATIO :',att_format)
+				worksheet.write(21, 0, 'REDO LOG HIT RATIO :',att_format)
+				worksheet.write(22, 0, 'NUMBER OF TRANSACTIONS :',att_format)
+				
+				worksheet.write(26, 0, 'TABLESPACE NAME',att_format2)
+				worksheet.write(26, 1, 'TOTAL SIZE(MB)',att_format2)
+				worksheet.write(26, 2, 'USED SPACE(MB)',att_format2)
+				worksheet.write(26, 3, 'AVAILABLE SPACE(MB)',att_format2)
+				
+				worksheet.write(1, 4, 'DATABASE',att_format)
+				worksheet.write(1, 5, 'SEQUENCE #',att_format)
+				worksheet.write(1, 6, 'ARCHIVED',att_format)
+				worksheet.write(1, 7, 'APPLIED',att_format)
+				worksheet.write(2, 4, 'PRIMARY :',att_format)
+				worksheet.write(3, 4, 'STANDBY :',att_format)
+				
+				worksheet.write('F8', 'BACKUP DATE',att_format)
+				worksheet.write('G8', 'CLIENT',att_format)
+				worksheet.write('H8', 'SCHEMA_NAME',att_format)
+				worksheet.write('I8', 'STATUS',att_format)
+				
+				worksheet.write('F17', 'FILESYSTEM',att_format)
+				worksheet.write('G17', 'TOTAL SIZE(GB)',att_format)
+				worksheet.write('H17', 'USED(GB)',att_format)
+				worksheet.write('I17', 'FREE(GB)',att_format)
+				worksheet.write('J17', 'USED(%)',att_format)
+				worksheet.write('K17', 'MOUNTED ON',att_format)
+				
+				for dbparameter in dbparameters:
+					if 'created' in dbparameter:
+						cursor.execute(dbparameter)
+						for ro in cursor.fetchone():
+							worksheet.write_datetime(4, 1, ro,date_format)
+							dbrow=dbrow+1
+					else:
+						cursor.execute(dbparameter)
+						for ro in cursor.fetchone():
+							worksheet.write(dbrow, 1, ro,att_out_format)
+							dbrow=dbrow+1
+		
+				for dbratio in dbratios:
+					cursor.execute(dbratio)
 					for ro in cursor.fetchone():
-						worksheet.write_datetime(4, 1, ro,date_format)
-						dbrow=dbrow+1
-				else:
-					cursor.execute(dbparameter)
+						worksheet.write(rrow, 1, ro,att_out_format)
+						rrow=rrow+1
+		
+				for dbtab in dbtabs:
+					if 'distinct' in dbtab:
+						cursor.execute(dbtab)
+						tcol=tcol+1
+						trow=28
+						for ro in cursor.fetchall():
+							worksheet.write(trow, tcol, ro[0],att_format)
+							trow=trow+1
+					else:
+						cursor.execute(dbtab)
+						tcol=tcol+1
+						trow=28
+						for ro in cursor.fetchall():
+							worksheet.write(trow, tcol, ro[0],att_out_format)
+							trow=trow+1
+		
+				for prsync in prsyncs:
+					syncol=syncol+1
+					cursor.execute(prsync)
 					for ro in cursor.fetchone():
-						worksheet.write(dbrow, 1, ro,att_out_format)
-						dbrow=dbrow+1
-	
-			for dbratio in dbratios:
-				cursor.execute(dbratio)
+						worksheet.write(2,syncol,ro,att_out_format) 
+		
+				for drsync in drsyncs:
+					drcol=drcol+1
+					drcursor.execute(drsync)
+					for ro in drcursor.fetchone():
+						worksheet.write(3,drcol,ro,att_out_format)
+		
+				cursor.execute(rmanstat)
 				for ro in cursor.fetchone():
-					worksheet.write(rrow, 1, ro,att_out_format)
-					rrow=rrow+1
-	
-			for dbtab in dbtabs:
-				if 'distinct' in dbtab:
-					cursor.execute(dbtab)
-					tcol=tcol+1
-					trow=28
-					for ro in cursor.fetchall():
-						worksheet.write(trow, tcol, ro[0],att_format)
-						trow=trow+1
-				else:
-					cursor.execute(dbtab)
-					tcol=tcol+1
-					trow=28
-					for ro in cursor.fetchall():
-						worksheet.write(trow, tcol, ro[0],att_out_format)
-						trow=trow+1
-	
-			for prsync in prsyncs:
-				syncol=syncol+1
-				cursor.execute(prsync)
-				for ro in cursor.fetchone():
-					worksheet.write(2,syncol,ro,att_out_format) 
-	
-		#	for drsync in drsyncs:
-		#		drcol=drcol+1
-		#		drcursor.execute(drsync)
-		#		for ro in drcursor.fetchone():
-		#			worksheet.write(3,drcol,ro,att_out_format)
-	
-			cursor.execute(rmanstat)
-			for ro in cursor.fetchone():
-				if ro == 'COMPLETED':
-					worksheet.merge_range('F7:I7',ro,merge_format_bkp_green)
-				else:
-					worksheet.merge_range('F7:I7',ro,merge_format_bkp_red)
-	
-			cursor.execute(dpbkpstat)
-			bcol=5
-			brow=8
-			for ro in cursor.fetchall():
-				worksheet.write_datetime(brow, 5, ro[0],date_format_bkp)
-				worksheet.write(brow, 6, ro[1],att_out_format)
-				worksheet.write(brow, 7, ro[2],att_out_format)
-				if ro[3] == 'COMPLETED':
-					worksheet.write(brow, 8, ro[3],bkp_format_green)
-				else:
-					worksheet.write(brow, 8, ro[3],bkp_format_red)
-				brow=brow+1
-	
-			cursor.close()
-			connection.close()
+					if ro == 'COMPLETED':
+						worksheet.merge_range('F7:I7',ro,merge_format_bkp_green)
+					else:
+						worksheet.merge_range('F7:I7',ro,merge_format_bkp_red)
+		
+				cursor.execute(dpbkpstat)
+				bcol=5
+				brow=8
+				for ro in cursor.fetchall():
+					worksheet.write_datetime(brow, 5, ro[0],date_format_bkp)
+					worksheet.write(brow, 6, ro[1],att_out_format)
+					worksheet.write(brow, 7, ro[2],att_out_format)
+					if ro[3] == 'COMPLETED':
+						worksheet.write(brow, 8, ro[3],bkp_format_green)
+					else:
+						worksheet.write(brow, 8, ro[3],bkp_format_red)
+					brow=brow+1
+		
+				cursor.close()
+				connection.close()
 
 workbook.close()
 print ("EOD BOD Checklist completed")
